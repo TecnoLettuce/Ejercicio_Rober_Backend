4757cb15bae58d4774df1affe9d70e691b773e5a
Gabriel <gabriel.sanchez@softwareone.com>
2021-09-01 14:12:58 +0200

    Add project files.

---
+++ Services/IBlobService.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Storage.Blobs;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+
+namespace Ejercicio_Rober_Backend.Services
+{
+    public interface IBlobService
+    {
+        Task<Uri> UploadFileBlobAsync(string blobContainerName, Stream content, string contentType, string fileName);
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// Dependencia de Azure Blobs
+using Azure.Storage.Blobs;
+using Ejercicio_Rober_Backend.Services;
+
+namespace Ejercicio_Rober_Backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configuración de Azure
+            services.AddScoped(x => new BlobServiceClient(Configuration.GetValue<string>("AzureBlobStorage")));
+            // Registro de la interfaz y el servicio creado 
+            services.AddScoped<IBlobService, BlobService>();
+
+            
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ejercicio_Rober_Backend", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ejercicio_Rober_Backend v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    } // Salida clase Startup
+}
